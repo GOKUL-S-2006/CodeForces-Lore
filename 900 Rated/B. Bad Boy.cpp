ec86430d98847038e1084cbd30f082ca8f5c892b
GOKUL-S-2006 <gokul.sathiyamurthi2006@gmail.com>
2025-05-19 21:02:15 +0530

    Create B. Bad Boy.cpp

---
+++ 900 Rated/B. Bad Boy.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m,i,j;
+        cin>>n>>m>>i>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+}
